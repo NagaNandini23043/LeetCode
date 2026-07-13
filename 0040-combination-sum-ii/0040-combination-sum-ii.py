09cbcfe0d02b26013f0bac4ee8693661c97f6fd6
NagaNandini23043 <naganandini84@gmail.com>
2026-07-13 22:10:50 +0530

    Time: 32 ms (51.05%), Space: 12.3 MB (97.18%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        result=[]
+
+        def back(start, comb, total):
+            if total==target:
+                result.append(comb[:])
+                return
+            
+            if total>target:
+                return
+            
+            for i in range(start, len(candidates)):
+                if i>start and candidates[i]==candidates[i-1]:
+                    continue
+
+                comb.append(candidates[i])
+                back(i+1,comb,total+candidates[i])
+                comb.pop()
+        
+        back(0,[],0)
+        return result
