d4045406b55f3365ae75021b456694d4b2a6c2f0
NagaNandini23043 <naganandini84@gmail.com>
2026-08-04 19:25:42 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.07%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        tab=[0]*(n+1)
-        if n>0:
-            tab[1]=1
-        if n>1:
-            tab[2]=1
+        if n==0:
+            return 0
         
-        if n>=3:
-            for i in range(3,n+1):
-                tab[i]=tab[i-1]+tab[i-2]+tab[i-3]
+        if n<=2:
+            return 1
         
-        return tab[n]
+        t0,t1,t2=0,1,1
+        
+        for _ in range(3,n+1):
+            t0,t1,t2=t1,t2,t0+t1+t2
+        
+        return t2
         
